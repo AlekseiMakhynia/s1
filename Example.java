4687799980d81ce553a3d3b8b2164c518419f3ca
Oleksiy Makhynia <makhynia.o.m@gmail.com>
2020-03-11 19:29:36 +0200

    first java program

---
+++ Example.java
@@ -0,0 +1,9 @@
+public class Example {
+    public static void main (String[] args) {
+        System.out.println("Firsr look on JAVA!");
+        // test for
+        for (int i=0; i<args.length; i++) {
+            System.out.println("arg i = " + i + " value = " + args[i]);
+        }
+    }
+}
---
+++ src/com/softserve/edu/Example.java
@@ -0,0 +1,11 @@
+package com.softserve.edu;
+
+public class Example {
+    public static void main (String[] args) {
+        System.out.println("Firsr look on JAVA!");
+        // test for
+        for (int i=0; i<args.length; i++) {
+            System.out.println("arg i = " + i + " value = " + args[i]);
+        }
+    }
+}
